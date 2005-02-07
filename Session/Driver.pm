946941fe517a657e191de62d20b918571a0e8500
sherzodr <sherzodr@4f332c09-35fb-0310-b6cc-8d808b03077a>
2005-02-07 07:17:21 +0000

    Driver specification updated. Standard error handling introduced.

--- Session/Driver.pm
+++ Session/Driver.pm
@@ -1,14 +1,15 @@
 package CGI::Session::Driver;
 
+# $Id$
+
 use strict;
 use diagnostics;
 
 use Carp "croak";
 use CGI::Session::ErrorHandler;
-use Data::Dumper;
 use vars qw( $VERSION @ISA );
 
-$VERSION = "0.01";
+$VERSION = "2.01";
 @ISA     = qw(CGI::Session::ErrorHandler);
 
 
@@ -53,3 +54,101 @@ sub dump {
 
 
 1;
+
+__END__;
+
+=pod
+
+=head1 NAME
+
+CGI::Session::Driver - CGI::Session driver specifications
+
+=head1 WARNING
+
+Version 2.01 of CGI::Session's driver specification is B<NOT> backward compatible with previous specification. If you already have a 
+driver developed to work with the previous version you're highly encouraged to upgrade your driver code to make it compatible with the
+current version. Fortunately, current driver specs are a lot easier to adapt to.
+
+If you need any help converting your driver to meet current specs, send me an e-mail. For support information see
+L<CGI::Session|CGI::Session>
+
+=head1 SYNOPSIS
+
+    require CGI::Session::Driver;
+    @ISA = qw( CGI::Session::Driver );
+
+=head1 DESCRIPTION
+
+CGI::Session::Driver is a base class for all CGI::Session's native drivers. It also documents driver specifications for those
+willing to write drivers for different databases not currently supported by CGI::Session.
+
+=head1 WHAT IS A DRIVER
+
+Driver is a piece of code that helps CGI::Session library to talk to specific database engines, or storage mechanisms. To be more
+precise, driver is a F<.pm> file that inherits from CGI::Session::Driver and defines L<retrieve()|/"retrieve()">, L<store()|/"store()"> and L<remove()|/"remove()"> methods.
+
+=head2 BLUEPRINT
+
+The best way of learning the specs is to look at a blueprint of a driver:
+
+    package CGI::Session::Driver::your_driver_name;
+    use strict;
+    use base qw( CGI::Session::Driver CGI::Session::ErrorHandler );
+
+    sub store {
+        my ($self, $sid, $datastr) = @_;
+        # Store $datastr, which is an already serialized string of data.
+        # Return any true value on success, undef failure.
+        # Set error message using $self->set_error()
+    }
+    
+    sub retrieve {
+        my ($self, $sid) = @_;
+        # Return $datastr, which was previously stored using above store() method.
+        # Return $datastr if $sid was found. Return 0 or "" if $sid doesn't exist
+        # in the datastore. Return undef to indicate failure. Set error message
+        # using $self->set_error()
+    }
+
+    sub remove {
+        my ($self, $sid) = @_;
+        # Remove storage associated for $sid. Return any true value indicating success,
+        # or undef on failure. Set error message using $self->set_error()
+    }
+
+All the attributes passed as the second argument to CGI::Session's new() or load() methods will automatically
+be made driver's object attributes. For example, if session object was initialized as following:
+
+    $s = CGI::Session->new("driver:your_driver_name", undef, {Directory=>'/tmp/sessions'});
+
+You can access value of 'Directory' from within your driver like so:
+
+    sub store {
+        my ($self, $sid, $datastr) = @_;
+        my $dir = $self->{Directory};   # <-- in this example will be '/tmp/sessions'
+    }
+
+Optionally, you can define C<init()> method within your driver to do driver specific global initialization. C<init()> method
+will be envoked only ones during the lifecycle of your driver, which is the same as the lifecycle of a session object.
+
+For examples of C<init()> look into native CGI::Session drivers.
+
+=head2 NOTES
+
+=over 4
+
+=item *
+
+All driver F<.pm> files must be lowercase!
+
+=item *
+
+DBI-related drivers are better off using L<CGI::Session::Driver::DBI|CGI::Session::Driver::DBI> as base, but don't have to.
+
+=back
+
+=head1 LICENSING
+
+For support and licensing information see L<CGI::Session|CGI::Session>.
+
+=cut
--- Session/ErrorHandler.pm
+++ Session/ErrorHandler.pm
@@ -1,17 +1,77 @@
 package CGI::Session::ErrorHandler;
 
+# $Id$
+
 use strict;
 use diagnostics;
+use vars qw( $VERSION );
 
-use vars qw( $ERRSTR );
+$VERSION = "1.01";
 
-sub error {
-    $ERRSTR = $_[1] || "";
+sub set_error {
+    my $class = shift;
+    $class = ref($class) || $class;
+    no strict 'refs';
+    ${ "$class\::errstr" } = $_[0] || "";
     return undef;
 }
 
+
+*error = \&errstr;
 sub errstr {
-    return $ERRSTR;
+    my $class = shift;
+    $class = ref( $class ) || $class;
+
+    no strict 'refs';
+    return ${ "$class\::errstr" };
 }
 
 1;
+
+__END__;
+
+=pod
+
+=head1 NAME
+
+CGI::Session::ErrorHandler - error handling routines for CGI::Session
+
+=head1 SYNOPSIS
+
+    require CGI::Session::ErrorHandler
+    @ISA = qw( CGI::Session::ErrorHandler );
+
+
+    sub some_method {
+        my $self = shift;
+        unless (  $some_condition ) {
+            return $self->set_error("some_method(): \$some_condition isn't met");
+        }
+    }
+
+
+=head1 DESCRIPTION
+
+CGI::Session::ErrorHandler provides set_error() and errstr() methods for setting and accessing error messages from within 
+CGI::Session's components. This method should be used by driver developers for providing CGI::Session-standard error 
+handling routines for their code
+
+=head2 METHODS
+
+=over 4
+
+=item set_error( $message )
+
+Implicitly defines $pkg_name::errstr and sets its value to $message. Return value is B<always> undef.
+
+=item errstr()
+
+Returns whatever value was set by the most recent call to set_error().
+
+=back
+
+=head1 LICENSING
+
+For support and licensing information see L<CGI::Session|CGI::Session>.
+
+=cut
