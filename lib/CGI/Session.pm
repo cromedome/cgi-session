d98a1717ee1b40280a399d6eea8b7e0bf8accb2f
sherzodr <sherzodr@4f332c09-35fb-0310-b6cc-8d808b03077a>
2006-03-28 12:52:36 +0000

    4.10 released to CPAN.

--- lib/CGI/Session.pm
+++ lib/CGI/Session.pm
@@ -7,7 +7,7 @@ use Carp;
 use CGI::Session::ErrorHandler;
 
 @CGI::Session::ISA      = qw( CGI::Session::ErrorHandler );
-$CGI::Session::VERSION  = '4.09';
+$CGI::Session::VERSION  = '4.10';
 $CGI::Session::NAME     = 'CGISESSID';
 $CGI::Session::IP_MATCH = 0;
 
