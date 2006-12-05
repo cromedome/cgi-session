d803128080cccd49c34f37fa9538ad66cad1ae38
markstos <markstos@4f332c09-35fb-0310-b6cc-8d808b03077a>
2006-12-05 02:10:19 +0000

    Version bumps for 4.20 release. No other changes.

--- lib/CGI/Session.pm
+++ lib/CGI/Session.pm
@@ -7,7 +7,7 @@ use Carp;
 use CGI::Session::ErrorHandler;
 
 @CGI::Session::ISA      = qw( CGI::Session::ErrorHandler );
-$CGI::Session::VERSION  = '4.20_1';
+$CGI::Session::VERSION  = '4.20';
 $CGI::Session::NAME     = 'CGISESSID';
 $CGI::Session::IP_MATCH = 0;
 
